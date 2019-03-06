b1180221cf45cea02c1a343c64fb14fffa6b0de0
jcardella@togllc.com <jcardella@togllc.com>
2019-03-06 10:34:46 -0500

    restored server renderToString, shelving server renderToNodeStream

---
+++ src/server/server-streaming-beta.js
@@ -0,0 +1,113 @@
+import '@babel/polyfill';
+import Express from 'express';
+import path from 'path';
+import React from 'react';
+import { renderToNodeStream } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ChunkExtractor } from '@loadable/server';
+import { Helmet } from 'react-helmet';
+import configureStore from '../shared/store/configureStore';
+import rootSaga from '../shared/sagas/rootSaga';
+import App from '../shared/containers';
+import serialize from 'serialize-javascript';
+
+const app = new Express();
+const port = 3000;
+
+
+app.use( '/assets/node', Express.static('dist/node') );
+app.use( '/assets/web', Express.static('dist/web') );
+
+const store = configureStore({});
+
+const renderHeader = () => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+      <title>Fix with async helmet</title>
+        <link rel="shortcut icon" href="https://s3.amazonaws.com/elite-website/images/favicon/favicon.ico"/>
+      </head>
+      <body>
+      <div id="app">
+  `;
+};
+
+const renderFooter = (loadableState, webExtractor) => {
+  return `
+    </div>
+    </body>
+    <script>window.__PRELOADED_STATE__ =${serialize((loadableState)).replace(/</g, '\\u003c')}</script>
+    ${webExtractor.getScriptTags()}
+    </html>
+  `;
+};
+
+const getWebExtractor = () => {
+  const webStats = path.resolve(__dirname, '../web/loadable-stats.json');
+  return new ChunkExtractor({ statsFile: webStats });
+};
+
+const createNodeStream = (req) => {
+  let webExtractor = getWebExtractor();
+
+  let context = {};
+
+  let jsx = webExtractor.collectChunks(
+    <Provider store={ store }>
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    </Provider>
+  );
+
+  if (context.url) {
+    res.redirect(context.url);
+    return;
+  }
+
+  return renderToNodeStream( jsx );
+};
+
+
+app.use( async (req, res) => {
+
+  let loadableState = {};
+
+  store.runSaga(rootSaga).toPromise()
+    .then( () => {
+
+      const htmlStream = createNodeStream( req )
+
+      res.set('Content-Type', 'text/html')
+      res.status(200).write( renderHeader() );
+
+      htmlStream.pipe( res, { end: false } );
+
+      htmlStream.on('data', (data) => {
+        console.log(serialize(data));
+        console.log('store.getState: ', store.getState())
+      });
+
+      htmlStream.on('end', () => {
+        loadableState = store.getState();
+        res.write( renderFooter(loadableState, getWebExtractor()) );
+        return res.end();
+        console.log('end occurred');
+      });
+
+    }).catch( e => res.status(500).send(e.message) );
+
+    store.close();
+
+});
+
+
+app.listen(process.env.PORT || port, (error) => {
+  if (error) {
+    console.error('error occurred: ', error);
+  } else {
+    console.info(`SERVER HAS STARTED.`)
+  }
+});
--- src/server/server.js
+++ src/server/server.js
@@ -2,106 +2,90 @@ import '@babel/polyfill';
 import Express from 'express';
 import path from 'path';
 import React from 'react';
-import { renderToNodeStream } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ChunkExtractor } from '@loadable/server';
 import { Helmet } from 'react-helmet';
+import { ChunkExtractor } from '@loadable/server';
+import serialize from 'serialize-javascript';
 import configureStore from '../shared/store/configureStore';
-// import configureStore from './store';
 import rootSaga from '../shared/sagas/rootSaga';
 import App from '../shared/containers';
-// import { renderHeader, renderFooter } from './render';
-import serialize from 'serialize-javascript';
+
 
 const app = new Express();
 const port = 3000;
 
-
 app.use( '/assets/node', Express.static('dist/node') );
 app.use( '/assets/web', Express.static('dist/web') );
 
-app.get( '*', (req, res) => {
+const store = configureStore({});
 
-  const url = req.url;
+const layout = (html, preloadedState, webExtractor) => {
+  const helmet = Helmet.renderStatic();
 
-  const store = configureStore();
+  return (
+  `
+    <!DOCTYPE html>
+      <html>
+        <head>
+          ${helmet.title.toString()}
+        </head>
+        <body>
+          <div id="app">${html}</div>
+        </body>
+        <script>window.__PRELOADED_STATE__ = ${serialize(preloadedState).replace(/</g, '\\x3c')}</script>
+        ${webExtractor.getScriptTags()}
+      </html>
+  `
+  );
+};
 
-  const context = {};
-
+const getWebExtractor = () => {
   const webStats = path.resolve(__dirname, '../web/loadable-stats.json');
-  const webExtractor = new ChunkExtractor({ statsFile: webStats });
+  return new ChunkExtractor({ statsFile: webStats });
+};
 
-  const appWithRouter = (
-    webExtractor.collectChunks(
-     <Provider store={ store }>
-       <StaticRouter location={url} context={context}>
-         <App />
-       </StaticRouter>
-     </Provider>
-   )
- );
+const sendContent = (req) => {
+  return layout(
+    createJsx( req ),
+    JSON.stringify( store.getState() ),
+    getWebExtractor()
+  );
+};
+
+const createJsx = (req) => {
+  let webExtractor = getWebExtractor();
+
+  let context = {};
+
+  let jsx = webExtractor.collectChunks(
+    <Provider store={ store }>
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    </Provider>
+  );
 
   if (context.url) {
-    console.log('context.url: ', context.url);
     res.redirect(context.url);
     return;
   }
 
-  let loadableState = {};
+  return renderToString( jsx );
+};
+
+
+app.get( '*', (req, res) => {
 
   store.runSaga(rootSaga).toPromise()
-    .then(() => {
-
-      console.log('store.getState(): ', store.getState());
-      const htmlStream = renderToNodeStream( appWithRouter );
-
-      res.set('Content-Type', 'text/html')
-      res.status(200).write(
-        `
-            <!DOCTYPE html>
-            <html>
-              <head>
-              <title>Fix with async helmet</title>
-                <link rel="shortcut icon" href="https://s3.amazonaws.com/elite-website/images/favicon/favicon.ico"/>
-              </head>
-              <body>
-              <div id="app">
-        `
-      );
-
-      htmlStream.pipe( res, { end: false } );
-
-      htmlStream.on('data', (data) => {
-        console.log(serialize(data));
-      });
-
-      htmlStream.on('end', () => {
-        loadableState = store.getState();
-        res.write(
-          `
-            </div>
-            </body>
-            <script>window.__PRELOADED_STATE__ =${serialize(renderState(loadableState)).replace(/</g, '\\u003c')}</script>
-            ${webExtractor.getScriptTags()}
-            </html>
-          `
-        );
-        return res.end();
-        console.log('end occurred');
-      });
-
-    })
+    .then( () => res.status(200).send( sendContent(req) ) )
     .catch( e => res.status(500).send(e.message) );
 
-    // need some way to trigger the saga so the data comes back and then can send with initial payload
-    // trigger a re-render // .replace(/</g, '\\u003c')
-    const renderState = (loadableState) => loadableState;
+    sendContent(req);
 
-    loadableState = renderState( store.getState() );
     store.close();
 
-    // resend the response
 });
 
 
