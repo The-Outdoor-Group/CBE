e96baf7f911541283bbd157b8d864f093ad7dadf
jcardella@togllc.com <jcardella@togllc.com>
2019-06-24 15:50:28 -0400

    fixed padding on slider image

--- dist/web/service-worker.js
+++ dist/web/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/assets/web/precache-manifest.df6d161c728036d21300b852deda92dd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/assets/web/precache-manifest.0e7c2fd0283882bad7a02ddfeffb99b3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // workbox.strategies.staleWhileRevalidate()
 
